3ef560bb5bc27c701d6405e4e5e6457534dc277f
Anwesh43 <anweshthecool0@gmail.com>
2019-11-13 19:32:59 +0530

    adding BBNode class

--- index.ts
+++ index.ts
@@ -97,3 +97,49 @@ class Animator {
 		}
 	}
 }
+
+class BBNode {
+		
+	state : State = new State()
+	prev : BBNode
+	next : BBNode
+
+	constructor(private i : number) {
+
+
+	}
+
+	addNeighbor() {
+		if (this.i < nodes - 1) {
+			this.next = new BBNode(this.i + 1)
+			this.next.prev = this
+		}
+	}
+
+	draw(context : CanvasRenderingContext2D) {
+		DrawingUtil.drawBBNode(context, this.i, this.state.scale)
+		if (this.next) {
+			this.next.draw(context)
+		}
+	}
+
+	update(cb : Function) {
+		this.state.update(cb)
+	}
+
+	startUpdating(cb : Function) {
+		this.state.startUpdating(cb)
+	}
+
+	getNext(dir : number, cb : Function) {
+		var curr : BBNode = this.prev 
+		if (dir == 1) {
+			curr = this.next 
+		}
+		if (curr) {
+			return curr 
+		}
+		cb()
+		return this
+	}
+}
