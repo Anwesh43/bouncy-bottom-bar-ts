27fd158d8faddc9b085f158828d9f0ffcbff62ad
Anwesh43 <anweshthecool0@gmail.com>
2019-11-13 19:34:55 +0530

    adding BarBouncy class

--- index.ts
+++ index.ts
@@ -143,3 +143,27 @@ class BBNode {
 		return this
 	}
 }
+
+class BarBouncy {
+		
+	root : BBNode = new BBNode(0)
+	curr : BBNode = this.root 
+	dir : number = 1
+
+	draw(context : CanvasRenderingContext2D) {
+		this.root.draw(context)
+	}
+
+	update(cb : Function) {
+		this.curr.update(() => {
+			this.curr = this.curr.getNext(this.dir, () => {
+				this.dir *= -1
+			})
+			cb()
+		})
+	}
+
+	startUpdating(cb : Function) {
+		this.curr.startUpdating(cb)
+	}
+}
